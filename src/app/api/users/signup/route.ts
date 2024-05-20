c7004c9eadc86adae821aa2283b995f8957fa96b
Nirosh Shetty <niroshshetty2020@gmail.com>
2024-05-20 22:56:00 +0530

    signup route updated

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,2 +1,21 @@
 import { connect } from "@/dbConfig/dbConfig";
-import { User } from "@/models/userModels";
+import User from "@/models/userModels";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+export async function POST(request: NextRequest) {
+  const reqBody = request.json();
+  const { username, email, password } = reqBody;
+  const user = await User.findOne({ email });
+  if (user) {
+    NextResponse.json({ error: "User already exits" }, { status: 400 });
+    return;
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  const savedUser = await newUser.save();
+}
